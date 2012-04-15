f0c4de6eb1a77e64f41195a3815503a447e00f13
Maximilian Balandat <Balandat@googlemail.com>
2012-04-15 13:16:14 -0700

    updated RPCA algorithm comparison functions

--- code/bench_synthetic.m
+++ code/bench_synthetic.m
@@ -4,29 +4,90 @@
 %   http://perception.csl.uiuc.edu/matrix-rank/sample_code.html
 
 % Copyright:   227A project group
-% Last edited:   Apr 09, 2012
+% Last edited:   Apr 14, 2012
 
 
 %% Generate random data
 
-
 % define parameters
-m = 20;
-n = 20;
-rk = 2;
+m = 100;
+n = 100;
+rk = 12;
 rho = 0.1; % support of error matrix S_0
+maxErr = 500; % bound on abs. value of errors
 
 % create a random matrix 
 L = randn(m,rk)/sqrt(m) * randn(rk,n)/sqrt(n);
-S = sign(sprandn(m,n,rho));
+tmpS = sprand(m,n,rho);
+S = maxErr*2*(tmpS-0.5*sign(tmpS));
 M = L + S;
 
-%% compute coherence parameter mu of L
+% compute coherence parameter mu of L
 mu = coherence(L);
 
+%% select which algorithms to run, set other options
 
-%% perform RPCA using different algorithms
+% set to zero if algorithm should not be used 
+options.intpt.active = 0;
+options.itth.active = 0;
+options.apg.active = 0;
+options.papg.active = 0;
+options.dpga.active = 0;
+options.ealm.active = 1;
+options.ialm.active = 1;
+
+% specify settings of the different algorithms
+options.intpt.tol = 'low'; % default: 'standard'
+options.itth.tol = 1e-4; % default: 5e-4 
+options.apg.tol = 1e-6; % default: 1e-6 
+options.papg.tol = 1e-6; % default: 1e-7 
+options.dpga.tol = 5e-6*norm(M,'fro'); % default: 2e-5*norm(M,'fro')
+options.dpga.maxIter = 2000; % default: 1000 
+options.ealm.tol = 1e-7; % default: 1e-7
+options.ialm.tol = 1e-7; % default: 1e-7
+
+
+
+%% perform RPCA
 lambda = 1/sqrt(m); % standard value
-results = rpca_wrapper(L,S,lambda);
+results = rpca_wrapper(L,S,lambda,options);
+
+
+%% get info for plotting
+names = fieldnames(results);
+runtimes = zeros(1,length(names));
+iterations = zeros(1,length(names));
+Lerrs = zeros(1,length(names));
+Serrs = zeros(1,length(names));
+for i=1:length(names)
+    algstruct = getfield(results,names{i});
+    runtimes(i) = algstruct.t_run;
+    iterations(i) = algstruct.numIter;
+    Lerrs(i) = algstruct.errL;
+    Serrs(i) = algstruct.errS;
+end
+
+%% tes BLWS implementation
+
+
+tic;
+[results.ialm.Lhat, results.ialm.Shat, results.ialm.numIter] = ...
+    inexact_alm_rpca(M,lambda);
+% compute relative errors
+results.ialm.errL = norm(L-results.ialm.Lhat,'fro')/norm(L,'fro');
+results.ialm.errS = norm(S-results.ialm.Shat,'fro')/norm(S,'fro');
+results.ialm.t_run = toc;
+
+
+tic;
+[results.ialm2.Lhat, results.ialm2.Shat, results.ialm2.numIter] = ...
+    BLWS_ialm_rpca(M,lambda);
+% compute relative errors
+results.ialm2.errL2 = norm(L-results.ialm2.Lhat,'fro')/norm(L,'fro');
+results.ialm2.errS2 = norm(S-results.ialm2.Shat,'fro')/norm(S,'fro');
+results.ialm2.t_run = toc;
+
+
+
 
 
--- code/rpca_wrapper.m
+++ code/rpca_wrapper.m
@@ -4,97 +4,118 @@
 %   problems through different algorithms. 
 
 % Copyright:   227A project group
-% Last edited:   Apr 09, 2012
+% Last edited:   Apr 14, 2012
 
 
-function results = rpca_wrapper(L,S,lambda)
+function results = rpca_wrapper(L,S,lambda,options)
 
     % extract information about problem
     [m,n] = size(L);
     M = L + S;
     
+   
     %% interior point on the dual (using cvx)
-    disp('Starting Interior Point Algorithm on the Dual');
-    tic;
-    flag = cvx_quiet(true);
-    cvx_begin        
-        variable Y(m,n);
-        maximize(trace(Y'*M));
-        subject to
-            norm(Y,2) <= 1;
-            norm(Y(:),Inf) <= 1;
-    cvx_end
-    cvx_quiet(flag);
-    % recover primal solution (TO DO!)
-    %results.intpt.Lhat = ;
-    %results.intpt.Shat = ;
-    % compute relative errors
-    %results.intpt.errL = ;
-    %results.intpt.errS = ;
-    
-    results.intpt.t_run = toc;
+    if options.intpt.active               
+        disp('Starting Interior Point Algorithm on the Dual');
+        tic;
+        flag = cvx_quiet(false);
+        cvx_precision(options.intpt.tol);
+        cvx_begin        
+            variable Y(m,n);
+            maximize(trace(Y'*M));
+            subject to
+                norm(Y,2) <= 1;
+                norm(Y(:),Inf) <= 1;
+        cvx_end
+        cvx_quiet(flag);
+        % recover primal solution (TO DO!)
+        %results.intpt.Lhat = ;
+        %results.intpt.Shat = ;
+        % compute relative errors
+        %results.intpt.errL = ;
+        %results.intpt.errS = ;
 
+        results.intpt.numIter = cvx_slvitr;
+        results.intpt.t_run = toc;
+    end
+    
 
     %% iterative thresholding
-    disp('Starting Iterative Thresholding Algorithm');
-    tic;
-    [results.itth.Lhat results.itth.Shat] = singular_value_rpca(M,lambda);
-    % compute relative errors
-    results.itth.errL = norm(L-results.itth.Lhat,'fro')/norm(L,'fro');
-    results.itth.errS = norm(S-results.itth.Shat,'fro')/norm(S,'fro');
-    
-    results.itth.t_run = toc;
+    if options.itth.active 
+        disp('Starting Iterative Thresholding Algorithm');
+        tic;
+        [results.itth.Lhat, results.itth.Shat, ~, results.itth.numIter] = ...
+            singular_value_rpca(M,lambda,options.itth.tol);
+        % compute relative errors
+        results.itth.errL = norm(L-results.itth.Lhat,'fro')/norm(L,'fro');
+        results.itth.errS = norm(S-results.itth.Shat,'fro')/norm(S,'fro');
+        results.itth.t_run = toc;
+    end
 
     
     %% accelerated proximal gradient
-    disp('Starting APG Algorithm');
-    tic;
-    [results.apg.Lhat results.apg.Shat] = proximal_gradient_rpca(M,lambda);
-    % compute relative errors
-    results.apg.errL = norm(L-results.apg.Lhat,'fro')/norm(L,'fro');
-    results.apg.errS = norm(S-results.apg.Shat,'fro')/norm(S,'fro');
+    if options.apg.active 
+        disp('Starting APG Algorithm');
+        tic;
+        [results.apg.Lhat, results.apg.Shat, results.apg.numIter] = ...
+            proximal_gradient_rpca(M,lambda,-1,options.apg.tol);
+        % compute relative errors
+        results.apg.errL = norm(L-results.apg.Lhat,'fro')/norm(L,'fro');
+        results.apg.errS = norm(S-results.apg.Shat,'fro')/norm(S,'fro');
+        results.apg.t_run = toc;
+    end
     
-    results.apg.t_run = toc;
     
     %% accelerated proximal gradient (partial SVD)
-    disp('Starting APG Algorithm using partial SVD');
-    tic;
-    [results.papg.Lhat results.papg.Shat] = partial_proximal_gradient_rpca(M,lambda);
-    % compute relative errors
-    results.papg.errL = norm(L-results.papg.Lhat,'fro')/norm(L,'fro');
-    results.papg.errS = norm(S-results.papg.Shat,'fro')/norm(S,'fro');
+    if options.papg.active 
+        disp('Starting APG Algorithm using partial SVD');
+        tic;
+        [results.papg.Lhat, results.papg.Shat, results.papg.numIter] = ...
+            partial_proximal_gradient_rpca(M,lambda,-1,options.papg.tol);
+        % compute relative errors
+        results.papg.errL = norm(L-results.papg.Lhat,'fro')/norm(L,'fro');
+        results.papg.errS = norm(S-results.papg.Shat,'fro')/norm(S,'fro');
+        results.papg.t_run = toc;
+    end
     
-    results.papg.t_run = toc;
     
     %% dual projected gradient ascent
-    disp('Starting Dual Projected Gradient Ascent Algorithm');
-    tic;
-    [results.dpga.Lhat results.dpga.Shat] = dual_rpca(M,lambda);
-    % compute relative errors
-    results.dpga.errL = norm(L-results.dpga.Lhat,'fro')/norm(L,'fro');
-    results.dpga.errS = norm(S-results.dpga.Shat,'fro')/norm(S,'fro');
+    if options.dpga.active 
+        disp('Starting Dual Projected Gradient Ascent Algorithm');
+        tic;
+        [results.dpga.Lhat, results.dpga.Shat, ~, results.dpga.numIter] = ...
+            dual_rpca(M,lambda,options.dpga.tol,options.dpga.maxIter);
+        % compute relative errors
+        results.dpga.errL = norm(L-results.dpga.Lhat,'fro')/norm(L,'fro');
+        results.dpga.errS = norm(S-results.dpga.Shat,'fro')/norm(S,'fro');
+        results.dpga.t_run = toc;
+    end
     
-    results.dpga.t_run = toc;
     
     %% augmented Lagrangian method (exact)
-    disp('Starting Exact ALM Algorithm');
-    tic;
-    [results.ealm.Lhat results.ealm.Shat] = exact_alm_rpca(M,lambda);
-    % compute relative errors
-    results.ealm.errL = norm(L-results.ealm.Lhat,'fro')/norm(L,'fro');
-    results.ealm.errS = norm(S-results.ealm.Shat,'fro')/norm(S,'fro');
+    if options.ealm.active 
+        disp('Starting Exact ALM Algorithm');
+        tic;
+        [results.ealm.Lhat, results.ealm.Shat, results.ealm.numIter] = ...
+            exact_alm_rpca(M,lambda);
+        % compute relative errors
+        results.ealm.errL = norm(L-results.ealm.Lhat,'fro')/norm(L,'fro');
+        results.ealm.errS = norm(S-results.ealm.Shat,'fro')/norm(S,'fro'); 
+        results.ealm.t_run = toc;
+    end
     
-    results.ealm.t_run = toc;
     
     %% augmented Lagrangian method (inexact)
-    disp('Starting Inexact ALM Algorithm');
-    tic;
-    [results.ialm.Lhat results.ialm.Shat] = inexact_alm_rpca(M,lambda);
-    % compute relative errors
-    results.ialm.errL = norm(L-results.ialm.Lhat,'fro')/norm(L,'fro');
-    results.ialm.errS = norm(S-results.ialm.Shat,'fro')/norm(S,'fro');
-    
-    results.ialm.t_run = toc;
+    if options.ialm.active          
+        disp('Starting Inexact ALM Algorithm');
+        tic;
+        [results.ialm.Lhat, results.ialm.Shat, results.ialm.numIter] = ...
+            inexact_alm_rpca(M,lambda);
+        % compute relative errors
+        results.ialm.errL = norm(L-results.ialm.Lhat,'fro')/norm(L,'fro');
+        results.ialm.errS = norm(S-results.ialm.Shat,'fro')/norm(S,'fro');
+        results.ialm.t_run = toc;
+    end
 
 
 end
